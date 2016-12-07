8330bbe3655931ded29b10d7b4c97b2e5918789c
canisater <canis.4ter@gmail.com>
2016-12-07 21:39:00 +0000

    Use OSM data

--- client/app/maps/maps.controller.js
+++ client/app/maps/maps.controller.js
@@ -4,16 +4,24 @@
 
 class MapsComponent {
 
-  	constructor($scope) {
-    	this.$scope = $scope;
-  	}
+  constructor($scope) {
+    this.$scope = $scope;
+	}
 
 
 	$onInit(){
-		var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+		var mymap = L.map('mapid').setView([51.0, -1.5], 11);
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
+    var marker = L.marker([51.0, -1.5]).addTo(mymap);
+
+    mymap.on('click', this.onMapClick);
+
 	}
 
-	
+  onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+  }
 
 }
 
